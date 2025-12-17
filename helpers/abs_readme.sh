987ef167a148bc60314d3063e32d097385648320
Matteo Nastasi <nastasi@openquake.org>
2025-12-17 17:00:51 +0100

    typo fixed

--- helpers/abs_readme.sh
+++ helpers/abs_readme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PKG_VERS="$1"
-sed -i 's@](\./LICENSE)@](https://github.com/gem/oq-gem-taxonomy-data/blob/'"$PKG_VERS"'/LICENSE)@g;s](\./DATA_LICENSE)@](https://github.com/gem/oq-gem-taxonomy-data/blob/'"$PKG_VERS"'/DATA_LICENSE)@g' README.md
+sed -i 's@](\./LICENSE)@](https://github.com/gem/oq-gem-taxonomy-data/blob/'"$PKG_VERS"'/LICENSE)@g;s@](\./DATA_LICENSE)@](https://github.com/gem/oq-gem-taxonomy-data/blob/'"$PKG_VERS"'/DATA_LICENSE)@g' README.md
